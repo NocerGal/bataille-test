fc3fa036ade39199fac6c631aa6fadd123a6fc40
NocerGal <luc.schenherr@hotmail.fr>
2024-06-09 09:25:09 +0200

    Correction logique métier

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿int turn = 1;
-bool isTeamBlueTurn = true;
-bool isTeamBlueWonBattle = true;
-string TeamBlue = "Empire";
-string TeamRed = "Rebels";
+bool isBlueTeamTurn = true;
+bool isBlueTeamWonBattle = true;
+string BlueTeam = "Empire";
+string RedTeam = "Rebels";
 
 int minAttackPower = 100;
 int maxAttackPower = 500;
@@ -15,46 +15,67 @@ if (int.Parse(args[0]) == 0 || int.Parse(args[1]) == 0)
     return;
 }
 
-Team empire = FactoryTeam.Create<EmpireSoldier>(int.Parse(args[0]), TeamBlue, minAttackPower, maxAttackPower, minHp, maxHp);
-Team rebels = FactoryTeam.Create<RebelsSoldier>(int.Parse(args[1]), TeamRed, minAttackPower, maxAttackPower, minHp, maxHp);
+Team empire = FactoryTeam.Create<EmpireSoldier>(int.Parse(args[0]), BlueTeam, minAttackPower, maxAttackPower, minHp, maxHp);
+Team rebels = FactoryTeam.Create<RebelsSoldier>(int.Parse(args[1]), RedTeam, minAttackPower, maxAttackPower, minHp, maxHp);
 
 List<ISoldier> aliveSoldiersEmpire = empire.Soldiers.Where(soldier => soldier.Hp > 0).ToList();
 List<ISoldier> aliveSoldiersRebels = rebels.Soldiers.Where(soldier => soldier.Hp > 0).ToList();
 
 Random random = new();
 
+if (isBlueTeamTurn)
+{
+
+    Console.WriteLine($"{BlueTeam} is more likely to win the battle!");
+    Console.WriteLine($"Round {turn}");
+    ISoldier soldierEmpire = aliveSoldiersEmpire[random.Next(aliveSoldiersEmpire.Count)];
+    ISoldier soldierRebels = aliveSoldiersRebels[random.Next(aliveSoldiersRebels.Count)];
+    soldierEmpire.Attack(soldierRebels);
+    isBlueTeamTurn = !isBlueTeamTurn;
+    turn++;
+}
+else
+{
+    isBlueTeamWonBattle = !isBlueTeamWonBattle;
+    Console.WriteLine($"{RedTeam} is more likely to win the battle!");
+    Console.WriteLine($"Round {turn}");
+    ISoldier soldierEmpire = aliveSoldiersEmpire[random.Next(aliveSoldiersEmpire.Count)];
+    ISoldier soldierRebels = aliveSoldiersRebels[random.Next(aliveSoldiersRebels.Count)];
+    soldierRebels.Attack(soldierEmpire);
+    isBlueTeamTurn = !isBlueTeamTurn;
+    turn++;
+}
+
 while (aliveSoldiersEmpire.Count > 0 && aliveSoldiersRebels.Count > 0)
 {
     Console.WriteLine($"Round {turn}");
-    if (isTeamBlueTurn)
+    if (isBlueTeamTurn)
     {
-
-        Console.WriteLine($"{TeamBlue} is more likely to win the battle!");
         ISoldier soldierEmpire = aliveSoldiersEmpire[random.Next(aliveSoldiersEmpire.Count)];
         ISoldier soldierRebels = aliveSoldiersRebels[random.Next(aliveSoldiersRebels.Count)];
         soldierEmpire.Attack(soldierRebels);
+        isBlueTeamTurn = !isBlueTeamTurn;
     }
     else
     {
-        isTeamBlueWonBattle = !isTeamBlueWonBattle;
-        Console.WriteLine($"{TeamRed} is more likely to win the battle!");
         ISoldier soldierEmpire = aliveSoldiersEmpire[random.Next(aliveSoldiersEmpire.Count)];
         ISoldier soldierRebels = aliveSoldiersRebels[random.Next(aliveSoldiersRebels.Count)];
         soldierRebels.Attack(soldierEmpire);
+        isBlueTeamTurn = !isBlueTeamTurn;
     }
     aliveSoldiersEmpire = empire.Soldiers.Where(soldier => soldier.Hp > 0).ToList();
     aliveSoldiersRebels = rebels.Soldiers.Where(soldier => soldier.Hp > 0).ToList();
     turn++;
-    isTeamBlueTurn = !isTeamBlueTurn;
+    isBlueTeamWonBattle = !isBlueTeamWonBattle;
 }
 
 if (aliveSoldiersEmpire.Count > 0)
 {
-    string wonAsSuposed = isTeamBlueWonBattle ? "as planned" : "";
-    Console.WriteLine($"{TeamBlue} won {wonAsSuposed}!");
+    string wonAsSuposed = isBlueTeamWonBattle ? "as planned" : "";
+    Console.WriteLine($"{BlueTeam} won {wonAsSuposed}!");
 }
 else
 {
-    string wonAsSuposed = isTeamBlueWonBattle ? "" : "as planned";
-    Console.WriteLine($"{TeamRed} won!");
+    string wonAsSuposed = isBlueTeamWonBattle ? "" : "as planned";
+    Console.WriteLine($"{RedTeam} won!");
 }
--- obj/Debug/net8.0/bataille.AssemblyInfo.cs
+++ obj/Debug/net8.0/bataille.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bataille")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3faf709054ec1a5dda40b3d19d154a22f71f0d7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac2dfc16d308d941446c91960d3fb1cb00c3f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("bataille")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bataille")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
