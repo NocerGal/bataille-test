cadf6a8b95bfcb8a1ecce58705aa6855e24c3c67
NocerGal <luc.schenherr@hotmail.fr>
2024-06-09 11:07:52 +0200

    Correction du calcule du score de l'équipe

--- team/Team.cs
+++ team/Team.cs
@@ -2,12 +2,20 @@ public class Team
 {
     public string Name { get; }
     public List<ISoldier> Soldiers { get; }
-    public int Score { get; private set; }
+    public int Score { get; set; }
 
     public Team(string name, List<ISoldier> soldiers)
     {
         Name = name;
         Soldiers = soldiers ?? new List<ISoldier>();
-        Score = Soldiers.Sum(soldier => soldier.Hp);
+        Score = Soldiers.Sum(soldier =>
+        {
+            int soldierHp = soldier.Hp;
+            int soldierAttackPower = soldier.AttackPower;
+            int rate = 10;
+            int calculatedScore = (soldierHp + soldierAttackPower) * rate;
+
+            return calculatedScore;
+        });
     }
 }
